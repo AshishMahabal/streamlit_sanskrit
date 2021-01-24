feac05dab0f15bddc2317c9900eeb58aefb36878
Ashish Mahabal <aam@astro.caltech.edu>
2021-01-23 17:54:13 -0800

    added debug

--- Lingua_SA_st.py
+++ Lingua_SA_st.py
@@ -11,19 +11,26 @@ sup = ["सु","औ","जस्","अम्","औट्","शस्","टा",'
     'ङसि','भ्याम्  ','भ्यस् ','ङस्','ओस्','आम्','ङि','ओस् ','सुप्',"सु ","औ ","जस् "]
 
 tests = {}
-tests['rAma'] = 'राम:#रामौ#रामा:#रामम्#रामौ#रामान्#रामेण#रामाभ्याम्#रामै:#रामाय#रामाभ्याम्#रामेभ्य:#\
-रामात्#रामाभ्याम्#रामेभ्य:#रामस्य#रामयो:#रामाणाम्#रामे#रामयो:#रामेषु#हे राम#हे रामौ#हे रामा:'
-tests['ramA'] = 'रमा#रमे#रमा:#रमाम्#रमे#रमा:#रमया#रमाभ्याम्#रमाभि:#रमायै#रमाभ्याम्#रमाभ्या:#\
-रमाया:#रमाभ्याम्#रमाभ्या:#रमाया:#रमयो:#रमाणाम्#रमायाम्#रमयो:#रमासु#हे रमे#हे रमे#हे रमा:'
-tests['hari'] = 'हरि:#हरी#हरय:#हरिम्#हरी#हरीन्#हरिणा#हरिभ्याम्#हरिभि:#हरये#हरिभ्याम्#हरिभ्य:#\
-हरे:#हरिभ्याम्#हरिभ्य:#हरे:#हर्यो:#हरीणाम्#हरौ#हर्यो:#हरिषु#हे हरे#हे हरी#हे हरय:'
-
+tests['ramA'] = 'प्रथमा#रमा#रमे#रमाः#द्वितीया#रमाम्#रमे#रमाः#तृतीया#रमया#रमाभ्याम्#रमाभिः#चतुर्थी#रमायै#रमाभ्याम्#रमाभ्याः#\
+पंचमी#रमायाः#रमाभ्याम्#रमाभ्याः#षष्ठी#रमायाः#रमयोः#रमाणाम्#सप्तमी#रमायाम्#रमयोः#रमासु#संबोधन#हे रमे#हे रमे#हे रम'
+tests['phala'] = 'प्रथमा#फलम्#फले#फलानि#द्वितीया#फलम्#फले#फलानि#तृतीया#फलेन#फलाभ्याम्#फलैः#\
+चतुर्थी#फलाय#फलाभ्याम्#फलेभ्यः#पंचमी#फलात्#फलाभ्याम्#फलेभ्यः#षष्ठी#फलस्य#फलयोः#फलानाम्#सप्तमी#फले#फलयोः#फलेषु#संबोधन#हे फल#हे फले#हे फलान'
+tests['hari'] = 'प्रथमा#हरिः#हरी#हरयः#द्वितीया#हरिम्#हरी#हरीन्#तृतीया#हरिणा#हरिभ्याम्#हरिभिः#चतुर्थी#हरये#हरिभ्याम्#हरिभ्यः#\
+पंचमी#हरेः#हरिभ्याम्#हरिभ्यः#षष्ठी#हरेः#हर्योः#हरीणाम्#सप्तमी#हरौ#हर्योः#हरिषु#संबोधन#हे हरे#हे हरी#हे हरय'
+tests['rAma'] = 'प्रथमा#रामः#रामौ#रामाः#द्वितीया#रामम्#रामौ#रामान्#तृतीया#रामेण#रामाभ्याम्#रामैः#\
+चतुर्थी#रामाय#रामाभ्याम्#रामेभ्यः#पंचमी#रामात्#रामाभ्याम्#रामेभ्यः#षष्ठी#रामस्य#रामयोः#रामाणाम्#सप्तमी#रामे#रामयोः#रामेषु#संबोधन#हे राम#हे रामौ#हे राम'
+tests['bAlaka'] = 'प्रथमा#बालकः#बालकौ#बालकाः#द्वितीया#बालकम्#बालकौ#बालकान्#तृतीया#बालकेन#बालकाभ्याम्#बालकैः#\
+चतुर्थी#बालकाय#बालकाभ्याम्#बालकेभ्यः#पंचमी#बालकात्#बालकाभ्याम्#बालकेभ्यः#षष्ठी#बालकस्य#बालकयोः#बालकानाम्#सप्तमी#बालके#बालकयोः#बालकेषु#संबोधन#हे बालक#हे बालकौ#हे बालकाः'
+tests['guru'] = 'प्रथमा#गुरुः#गुरू#गुरवः#द्वितीया#गुरुम्#गुरू#गुरून्#तृतीया#गुरुणा#गुरुभ्याम्#गुरुभिः#\
+चतुर्थी#गुरवे#गुरुभ्याम्#गुरुभ्यः#पंचमी#गुरोः#गुरुभ्याम्#गुरुभ्यः#षष्ठी#गुरोः#गुर्वोः#गुरूणाम्#सप्तमी#गुरौ#गुर्वोः#गुरुषु#संबोधन#हे गुरो#हे गुरू#हे गुरव'
 
 linga = {
         'rAma':'puM',
+        'bAlaka':'puM',
         'phala':'napuMsaka',
         'ramA':'strI',
         'hari':'puM',
+        'nadi':'strI',
         'guru':'puM',
 #        'dhenu':'strI',
 #        'madhu':'napuMsaka',
@@ -62,6 +69,11 @@ if st.sidebar.checkbox('Show nountable'):
     df.set_index('', inplace=True)
     st.write(df.to_markdown())
 
+if st.sidebar.checkbox('Create nountable data'):
+    df = nountable(noun,linga[noun])
+    st.write("tests['",noun,"'] = ")
+    st.write('#'.join([j for i in df.values.tolist() for j in i]))
+
 if st.sidebar.checkbox('Quiz nountable'):
     st.write("Please complete the following for ",noun)
     vibhaktis = []
@@ -72,15 +84,24 @@ if st.sidebar.checkbox('Quiz nountable'):
             vibhaktis.append(cols[j+1].text_input(sup[i*3+j],""))
 
     cvibhaktis = tests[noun].split('#')
+    #cvibhaktis = list(map(lambda it: it.strip(), cvibhaktis))
+    cvibhaktis = [item.strip() for item in cvibhaktis]
 
     for i in range(len(labels)):
         for j in range(len(vachanas)):
-            if cvibhaktis[i*3+j] != vibhaktis[i*4+j+1]:
+            if cvibhaktis[i*4+j+1] != vibhaktis[i*4+j+1].strip():
                 st.write("Mismatch in ",labels[i],vachanas[j])
 
 if st.sidebar.checkbox('Debug'):
     for i in range(len(labels)):
         for j in range(len(vachanas)):
-            if cvibhaktis[i*3+j] != vibhaktis[i*4+j+1]:
+            if cvibhaktis[i*4+j+1] != vibhaktis[i*4+j+1].strip():
                 st.write("Mismatch in ",labels[i], vachanas[j],
-                " actual: ",cvibhaktis[i*3+j], " user: ", vibhaktis[i*3+j],)
+                " actual: ",cvibhaktis[i*4+j+1], " user: ", vibhaktis[i*4+j+1],)
+
+if st.sidebar.checkbox('Debug2'):
+    for i in range(len(labels)):
+        for j in range(len(vachanas)):
+            if cvibhaktis[i*4+j+1] != vibhaktis[i*4+j+1].strip():
+                st.write("Mismatch in ",labels[i], vachanas[j],
+                " actual: ",cvibhaktis[i*4+j+1].encode(), " user: ", vibhaktis[i*4+j+1].encode())
